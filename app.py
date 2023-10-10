48f5c0c0a8a79b82f34c310d5ad04f9d8916a4f8
vkwai <vindollarsk@gmail.com>
2023-10-10 14:49:30 -0700

    Add user, admin routes

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import *
 from chat_request import text_request
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-@app.route("/",methods=("GET","POST"))
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/chat',methods=("GET","POST"))
 def chat():
     if request.method == "POST":
         user_in = request.form["chatbox"]
@@ -14,4 +19,25 @@ def chat():
         return redirect(url_for("chat",result=response))
 
     result = request.args.get("result")
-    return render_template("chat.html", result=result)
+    return render_template("chat.html", result=result, userResponse=session['username'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Handle the login form submission
+        username = request.form['username']
+        # Perform authentication and validation here
+        # Redirect to a new page on successful login
+        session['username'] = username
+        if (username == 'admin'):
+            return redirect(url_for('admin'))
+        return redirect(url_for('chat'))
+
+    # Render the login page for GET requests
+    return render_template('login.html')
+
+@app.route('/admin')
+def admin():
+    if session['username'] == 'admin':
+        return render_template('admin.html')
+    return redirect(url_for('/'))
