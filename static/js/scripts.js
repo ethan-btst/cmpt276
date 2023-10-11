9bbc97dab4019c3d8b007b6696ae7a80c9051acd
Ethan Bautista <ethanbtst@gmail.com>
2023-10-10 21:28:14 -0700

    Added dynamic usage notes in /chat

---
+++ static/js/scripts.js
@@ -0,0 +1,27 @@
+
+// Text that replaces usage notes in JSON form
+
+// Keep name the same as found in "value"
+// <select name="type" id="request_type">
+//                <option value="text">text</option> 
+//                <option value="youtube"...
+var usage_notes ={
+    "text" : "General chatgpt request, need to write 'Summarize the bible' in full",
+
+    "youtube" : "Use only the the youtube id <br> " + 
+                "example: \"www.youtube.com/watch?v=bBQVR4epfBQ&pp=ygUScGVydW4gcmVxdWlyZW1lbnRz\" <br> " +
+                "becomes: \"bBQVR4epfBQ&pp=ygUScGVydW4gcmVxdWlyZW1lbnR\"",
+
+    "article" : "Only applicable to CNN articles as of now",
+}
+
+let request_type = document.getElementById("request_type")
+
+// Changes usage notes
+function change_notes(){
+    document.getElementById("usage notes").innerHTML = usage_notes[request_type.value]
+}
+request_type.addEventListener("change",change_notes)
+
+// Initial loading of usage notes 
+change_notes()
