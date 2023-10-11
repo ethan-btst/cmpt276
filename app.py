b084d17557257e359ad0cb57e06cc0c22ea38108
Ethan Bautista <ethanbtst@gmail.com>
2023-10-11 10:52:36 -0700

    Added comments and a 404 error page

--- app.py
+++ app.py
@@ -4,35 +4,12 @@ from chat_request import text_request
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+# Homepage, redirects to login page
 @app.route('/')
 def index():
     return render_template('login.html')
 
-@app.route('/chat',methods=("GET","POST"))
-def chat():
-
-    # Redirect if not logged in
-    if 'username' not in session:
-        return redirect("login")
-
-    # Set a default empty response
-    if 'current_response' not in session:
-        session["current_response"] = '' 
-
-    if request.method == "POST":
-        user_in = request.form["chatbox"]
-        type = request.form["type"]
-        api_key = request.form["api_key"]
-
-        session["current_response"] = text_request(user_in,type,api_key)
-        return redirect(request.path)
-
-    if request.method == "GET":
-        return render_template("chat.html",result=session['current_response'],userResponse=session['username'])
-
-    result = request.args.get("result")
-    return render_template("chat.html", result=result, userResponse=session['username'])
-
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -48,8 +25,41 @@ def login():
     # Render the login page for GET requests
     return render_template('login.html')
 
+# Admin page
 @app.route('/admin')
 def admin():
     if session['username'] == 'admin':
         return render_template('admin.html')
     return redirect(url_for('/'))
+
+# 404 error page
+@app.errorhandler(404)
+def notfound(e):
+    return render_template("404.html")
+
+# Page for chat
+@app.route('/chat',methods=("GET","POST"))
+def chat():
+
+    # Redirect if not logged in
+    if 'username' not in session:
+        return redirect("login")
+
+    # Set a default empty response
+    if 'current_response' not in session:
+        session["current_response"] = '' 
+
+    # Processes chat request
+    if request.method == "POST":
+        user_in = request.form["chatbox"]
+        type = request.form["type"]
+        api_key = request.form["api_key"]
+
+        session["current_response"] = text_request(user_in,type,api_key)
+        return redirect(request.path)
+
+    # Display result to page
+    if request.method == "GET":
+        return render_template("chat.html",result=session['current_response'],userResponse=session['username'])
+
+    return render_template("chat.html", userResponse=session['username'])
